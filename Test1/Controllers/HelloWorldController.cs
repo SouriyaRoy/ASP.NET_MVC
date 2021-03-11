8b35711f9e563e2e560355cbd9d3da49bb895424
Souriya Roy <souriyaroy1998@gmail.com>
2021-03-11 15:22:54 +0530

    Add project files.

---
+++ Test1/Controllers/HelloWorldController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Public methods inside controllers are called Action
+        public string Test()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        // localhost/Helloworld/foo?name=Souriya
+        //public string Foo(string name)
+        //{
+        //    return "Hello "+name;
+        //}
+
+        // localhost/helloworld/foo?name1=Souriya&name2=Roy
+        public string Foo(string name1, string name2)
+        {
+            return "Hello " + name1 + ":" + name2;
+        }
+
+        // localhost/HelloWorld/bar/12
+        public string Bar(int id)
+        {
+            return id.ToString();
+        }
+
+        public string Shows(string id)
+        {
+            return id;
+        }
+
+        public ActionResult Display1()
+        {
+            return View(); //html+css
+        }
+
+        public ActionResult Display2()
+        {
+            var emp = new { id = 10, name = "Souriya", city = "Pune" };
+            return Json(emp, JsonRequestBehavior.AllowGet); //json format
+        }
+    }
+}
---
+++ Test1/Controllers/MoviesController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test1.Models;
+
+namespace Test1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        static List<Movie> allmovies = new List<Movie>()
+        {
+            new Movie()
+            {
+                ID = 1,
+                Title = "Star Wars",
+                ReleaseDate = DateTime.Now,
+                Genre = "Action",
+                Price = 100
+            },
+            new Movie()
+            {
+                ID = 2,
+                Title = "Jaws",
+                ReleaseDate = DateTime.Now,
+                Genre = "Action",
+                Price = 100
+            },
+            new Movie()
+            {
+                ID = 3,
+                Title = "Predator",
+                ReleaseDate = DateTime.Now,
+                Genre = "Action",
+                Price = 100
+            },
+            new Movie()
+            {
+                ID = 4,
+                Title = "Mandalorian",
+                ReleaseDate = DateTime.Now,
+                Genre = "Action",
+                Price = 100
+            },
+            new Movie()
+            {
+                ID = 5,
+                Title = "Matrix",
+                ReleaseDate = DateTime.Now,
+                Genre = "SciFi",
+                Price = 100
+            }
+        };
+        public ActionResult All()
+        {
+            return View(allmovies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = allmovies.Find(m => m.ID == id);
+            return View(movie);
+        }
+
+        //public ActionResult Edit()
+        //{
+
+        //}
+        //public ActionResult Delete()
+        //{
+
+        //}
+        //public ActionResult Create()
+        //{
+
+        //}
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(allmovies);
+        }
+    }
+}
